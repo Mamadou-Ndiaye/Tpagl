714f41b6f0324219b4319201ed67ef4ae88b108d
Mamadou-Ndiaye <ndiamamadou@gmail.com>
2019-11-23 00:17:35 +0000

    test

--- src/test/java/CalculTest.java
+++ src/test/java/CalculTest.java
@@ -7,22 +7,28 @@ import junit.framework.TestCase;
 
 public class CalculTest {
 	
-	private Calcul subject;
+	private Calcul c1;
 
 	@Before
 	public void setup() {
-		subject = new Calcul();
+		c1 = new Calcul();
 	}
 	
 	@Test
 	public void testAddition() {
-		assertEquals(5.0, subject.addition(2.0,3.0),0);
+		assertEquals(5.0, c1.addition(2.0,3.0),0);
+		assertEquals(7.0, c1.addition(4.0,3.0),0);
+		assertEquals(6.0, c1.addition(3.0,3.0),0);
+		assertEquals(9.0, c1.addition(6.0,3.0),0);
 	}
 
 	 
 	@Test
 	public void testMultiplication(){
-		assertEquals(8.0, subject.multiplication(2.0,4.0),0);
+		assertEquals(8.0, c1.multiplication(2.0,4.0),0);
+		assertEquals(10.0, c1.multiplication(2.0,5.0),0);
+		assertEquals(7.0, c1.multiplication(1.0,7.0),0);
+		assertEquals(0.0, c1.multiplication(0.0,4.0),0);
 	}
 
 }
